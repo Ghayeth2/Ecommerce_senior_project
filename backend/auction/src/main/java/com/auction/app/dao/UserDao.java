acd18a11763358c40ee7abaa1354f04fda04de13
Ghayeth Al Masri <99596025+Ghayeth2@users.noreply.github.com>
2023-10-25 15:55:53 +0300

    Delete backend/auction directory

--- backend/auction/src/main/java/com/auction/app/dao/UserDao.java
+++
@@ -1,7 +0,0 @@
-package com.auction.app.dao;
-
-import com.auction.app.model.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserDao extends JpaRepository<User, Long> {
-}
--- backend/auction/src/main/java/com/auction/app/service/abstracts/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.auction.app.service.abstracts;
-
-import com.auction.app.model.entities.User;
-
-import java.util.List;
-
-public interface UserService {
-    List<User> users();
-}
