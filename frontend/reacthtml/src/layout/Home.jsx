16252a226eefe399f57c8a1d4c770fa82dfd05e8
Ghayeth2 <amalj6570@gmail.com>
2023-10-29 22:18:21 +0300

    new state final templat

---
+++ frontend/reacthtml/src/layout/Home.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import Topbar from '../Components/Topbar'
+import Navbar from '../Components/Navbar'
+import Featured from '../Components/Featured'
+import Categories from '../Components/Categories'
+import Offer from '../Components/Offer'
+import Products from '../Components/Products'
+import Subscribe from '../Components/Subscribe'
+import JusArived from '../Components/JusArived'
+import Vendor from '../Components/Vendor'
+import Footer from '../Components/Footer'
+export default function 
+() {
+  return (
+    <div>
+<body>
+    <Topbar /> 
+    <Navbar />
+    <Featured /> 
+    <Categories />
+    <Offer /> 
+    <Products /> 
+    <Subscribe />
+    <JusArived />
+    <Vendor />
+    <Footer />
+
+</body>
+
+<a href="#" class="btn btn-primary back-to-top"><i class="fa fa-angle-double-up"></i></a>
+
+
+{/* <!-- JavaScript Libraries --> */}
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+<script src="lib/easing/easing.min.js"></script>
+<script src="lib/owlcarousel/owl.carousel.min.js"></script>
+
+{/* <!-- Contact Javascript File --> */}
+<script src="mail/jqBootstrapValidation.min.js"></script>
+<script src="mail/contact.js"></script>
+
+{/* <!-- Template Javascript --> */}
+<script src="js/main.js"></script>
+
+
+    </div>
+  )
+}
